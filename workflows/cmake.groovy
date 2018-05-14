31c9e708f3f0fefdb663e7ed6fa596ceab9ed6ce
Derek <derekisinternet@gmail.com>
2018-05-13 21:57:45 -0700

    bugfixes

--- workflows/cmake.groovy
+++ workflows/cmake.groovy
@@ -2,15 +2,15 @@ import com.concur.*
 
 public cMake(Map, yml, Map args) {
   def cMakeLists = args?.cMakeLists ?: yml?.tools?.cMake?.cMakeLists
-  def cmakeDir   = args?.cMakeDir   ?: yml?.tools?.cMake?.cMakeDir
+  def cMakeDir   = args?.cMakeDir   ?: yml?.tools?.cMake?.cMakeDir
 
   assert cMakeDir : "Workfkows :: cMake :: cMake :: [cMakeDir] needs to be defined."
-  sh "cmake ${cMakeDir}"
+  sh "cd ${cMakeDir} && cmake ."
 }
 
 public make(Map yml, Map args) {
   def makeDir = args?.makeDir ?: yml?.tools?.cMake?.cMakeDir
 
   assert makeDir : "Workfkows :: cMake :: cMake :: [makeDir] needs to be defined."
-  sh "make"
+  sh "cd ${makeDir} && make"
 }
