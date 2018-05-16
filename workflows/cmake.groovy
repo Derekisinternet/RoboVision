20b6e1eaa1bb6d556251ec37831cf6658a960cba
Derek <derekisinternet@gmail.com>
2018-05-15 20:53:37 -0700

    strong typing

--- workflows/cmake.groovy
+++ workflows/cmake.groovy
@@ -1,15 +1,17 @@
 import com.concur.*
 
 public cMake(Map, yml, Map args) {
-  def cMakeLists = args?.cMakeLists ?: yml?.tools?.cMake?.cMakeLists
-  def cMakeDir   = args?.cMakeDir   ?: yml?.tools?.cMake?.cMakeDir
+  String cMakeLists = args?.cMakeLists ?: yml?.tools?.cMake?.cMakeLists
+  String cMakeDir   = args?.cMakeDir   ?: yml?.tools?.cMake?.cMakeDir
+  String container  = args?.container  ?: yml?.tools?.cMake
 
   assert cMakeDir : "Workfkows :: cMake :: cMake :: [cMakeDir] needs to be defined."
   sh "cd ${cMakeDir} && cmake ."
 }
 
 public make(Map yml, Map args) {
-  def makeDir = args?.makeDir ?: yml?.tools?.cMake?.cMakeDir
+  String makeDir   = args?.makeDir   ?: yml?.tools?.cMake?.cMakeDir
+  String container = args?.container ?: yml?.tools?.container
 
   assert makeDir : "Workfkows :: cMake :: cMake :: [makeDir] needs to be defined."
   sh "cd ${makeDir} && make"
