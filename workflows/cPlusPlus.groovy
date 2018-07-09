8f4b11539236ec0a805bb3a7292b5f1a6f97f220
Derek <derekisinternet@gmail.com>
2018-07-08 20:34:15 -0700

    updates

--- workflows/cPlusPlus.groovy
+++ workflows/cPlusPlus.groovy
@@ -1,15 +1,15 @@
 import com.concur.*
 
 public cMake(Map yml, Map args) {
-  String cMakeLists = args?.cMakeLists ?: yml?.tools?.cPlusPlus?.cMakeLists
-  String cMakeDir   = args?.cMakeDir   ?: yml?.tools?.cPlusPlus?.cMakeDir
-  String container  = args?.container  ?: yml?.tools?.cPlusPlus?.container
+  String cMakeListsLocation = args?.cMakeLists    ?: yml?.tools?.cPlusPlus?.cMakeLists
+  String cMakeBuildDir      = args?.cMakeBuildDir ?: yml?.tools?.cPlusPlus?.cMakeBuildDir ?: '..'
+  String container          = args?.container     ?: yml?.tools?.cPlusPlus?.container
 
-  assert cMakeDir  : "Workfkows :: cMake :: cMake :: [cMakeDir] needs to be defined."
-  assert container : "Workfkows :: cMake :: cMake :: [container] needs to be defined."
+  assert cMakeBuildDir  : "Workfkows :: cPlusPlus :: cMake :: [cMakeDir] needs to be defined."
+  assert container : "Workfkows :: cMake :: ccPlusPlusMake :: [container] needs to be defined."
   
-  docker.image("$container").inside("--entyrpoint ''") {
-    sh "cd ${cMakeDir} && cmake ."
+  docker.image("$container").inside("--entrypoint ''") {
+    sh "cd ${cMakeBuildDir} && cmake ${cMakeListsLocation}"
   }
 }
 
@@ -17,10 +17,10 @@ public make(Map yml, Map args) {
   String makeDir   = args?.makeDir   ?: yml?.tools?.cMake?.cMakeDir
   String container = args?.container ?: yml?.tools?.container
 
-  assert makeDir   : "Workfkows :: cMake :: cMake :: [makeDir] needs to be defined."
-  assert container : "Workfkows :: cMake :: cMake :: [container] needs to be defined."
+  assert makeDir   : "Workfkows :: cPlusPlus :: make :: [makeDir] needs to be defined."
+  assert container : "Workfkows :: cPlusPlus :: make :: [container] needs to be defined."
 
-  docker.image("$container").inside("-u 0:0 --entyrpoint ''") {
+  docker.image("$container").inside("-u 0:0 --entrypoint ''") {
     sh "cd ${makeDir} && make"
   }
 }
