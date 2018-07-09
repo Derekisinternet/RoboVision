0329ae1e1856a913f0cf100cb23760ac13c21672
Derek <derekisinternet@gmail.com>
2018-07-08 20:18:38 -0700

    typo

--- workflows/cPlusPlus.groovy
+++ workflows/cPlusPlus.groovy
@@ -3,7 +3,7 @@ import com.concur.*
 public cMake(Map, yml, Map args) {
   String cMakeLists = args?.cMakeLists ?: yml?.tools?.cMake?.cMakeLists
   String cMakeDir   = args?.cMakeDir   ?: yml?.tools?.cMake?.cMakeDir
-  String container  = args?.container  ?: yml?.tools?.cMake
+  String container  = args?.container  ?: yml?.tools?.cMake?.container
 
   assert cMakeDir  : "Workfkows :: cMake :: cMake :: [cMakeDir] needs to be defined."
   assert container : "Workfkows :: cMake :: cMake :: [container] needs to be defined."
