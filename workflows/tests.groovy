418a271ddfbf612d1f580444a277354bf732d784
Derek <derekisinternet@gmail.com>
2018-07-14 00:22:46 -0700

    upgrade

--- workflows/tests.groovy
+++ workflows/tests.groovy
@@ -5,12 +5,23 @@ public test(Map yml, Map args) {
   String positiveSet = args?.positveSet  ?: yml.tools?.tests?.positiveSet
   String negativeSet = args?.negativeSet ?: yml.tools?.tests?.negativeSet
   String dockerImage = args?.dockerImage ?: yml.tools?.tests?.dockerImage
+  String cMakeListsLocation = args?.cMakeListsLocation  ?: '..'
+  String buildDir    = args?.buildDir ?: yml?.tools?.cPlusPlus?.buildDir
 
   assert exe : "workflows :: tests :: test :: [executable] required as a parameter"
+  assert dockerImage : "workflows :: tests :: test :: [dockerImage] required as a parameter"
   assert positiveSet : "workflows :: tests :: test :: [positiveSet] required as a parameter"
   assert negativeSet : "workflows :: tests :: test :: [negativeSet] required as a parameter"
+  assert buildDir    : "workflows :: tests :: test :: [buildDir] required as a parameter"
 
-  sh "${exe} -t ${positiveSet} -n ${negativeSet}"
+  String runString = "cd $buildDir"
+  runstring += " && cmake $cMakeListsLocation"
+  runstring += " && make ."
+  runString += " && ./$exe -t $positives -n $negatives"
+  docker.image(dockerImage).inside("-u 0:0 --entrypoint=''"){
+    sh runString
+  }
+  
 }
 
 return this;
