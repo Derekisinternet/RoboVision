f0fb37b11b83833c9f4c085c43557201523d299e
Derek <derekisinternet@gmail.com>
2018-07-18 19:19:41 -0700

    tightened makeCMakeLists.sh

--- makeCMakeLists.sh
+++ makeCMakeLists.sh
@@ -1,54 +1,26 @@
 #!/bin/sh
 # an automated way to make CMakeLists, because I'm lazy:
-
-if [ $# -eq 0 ]
+PROJECT="roboVision"       
+FILE=CMakeLists.txt
+if [ -e $FILE ]
     then
-        echo 'No Project name specified'
-    else
-        while [[ $# -gt 0 ]]
-        do
-            key="$1"
-
-            case $key in
-                -n|--name)
-                PROJECT="$2"
-                shift
-                shift
-                ;;
-                -r|--release)
-                PROJECT="roboVision"
-                shift
-                ;;
-            esac
-        done
-        
-        if [ -z $PROJECT ]
-            then
-                echo "Project name not set"
-                exit 1
-        fi
-        
-        FILE=CMakeLists.txt
-        if [ -e $FILE ]
-            then
-                rm $FILE
-        fi
-
-        OPENCV_LOCATION="$HOME/opencv/build"
-        if [ $PROJECT == roboVision ]
-            then
-                printf 'file (GLOB SOURCES\n    src/*.cpp\n)\n' >> $FILE
-        fi
-        printf 'cmake_minimum_required(VERSION 2.8)\n' >> $FILE
-        printf "project( $PROJECT )\n" >> $FILE
-        if [ $PROJECT == roboVision ]
-            then
-                printf 'include_directories( include )\n' >> $FILE
-        fi
-        printf "set(OpenCV_DIR $OPENCV_LOCATION)\n" >> $FILE
-        printf 'find_package( OpenCV REQUIRED )\n' >> $FILE
-        printf "add_executable( $PROJECT \${SOURCES} )\n" >> $FILE
-        printf "add_definitions(-std=c++0x)\n" >> $FILE
-        printf "target_link_libraries( $PROJECT" >> $FILE
-        printf ' ${OpenCV_LIBS} )\n'  >> $FILE
+        rm $FILE
 fi
+# Assumes the build location is set in an environment variable.
+OPENCV_LOCATION="$OPENCV_LOCATION"
+if [ $PROJECT == roboVision ]
+    then
+        printf 'file (GLOB SOURCES\n    src/*.cpp\n)\n' >> $FILE
+fi
+printf 'cmake_minimum_required(VERSION 2.8)\n' >> $FILE
+printf "project( $PROJECT )\n" >> $FILE
+if [ $PROJECT == roboVision ]
+    then
+        printf 'include_directories( include )\n' >> $FILE
+fi
+printf "set(OpenCV_DIR $OPENCV_LOCATION)\n" >> $FILE
+printf 'find_package( OpenCV REQUIRED )\n' >> $FILE
+printf "add_executable( $PROJECT \${SOURCES} )\n" >> $FILE
+printf "add_definitions(-std=c++0x)\n" >> $FILE
+printf "target_link_libraries( $PROJECT" >> $FILE
+printf ' ${OpenCV_LIBS} )\n'  >> $FILE
